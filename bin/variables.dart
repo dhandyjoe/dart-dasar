aceebf6d1c46459436e322c76eb3747bbd49831f
dhandyjoe <dhanjoenkp@gmail.com>
2021-08-08 01:02:21 +0700

    Initial commit

---
+++ bin/variables.dart
@@ -0,0 +1,20 @@
+void main() {
+
+  // Simple -> otomatis generate tipe data sesuai value
+  var name = "Dhandy";
+
+  // Specifying
+  String alamat = 'Blotongan';
+  int angka = 1;
+
+
+
+  // FINAL and CONSTS
+  // Final -> cant change the value
+  final age = 20;
+
+  // Consts
+  const status = true;
+
+
+}
